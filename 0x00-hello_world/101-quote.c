593f68928696e49985f966b96323f9c5104418ad
Yommywhales <yommywhales@gmail.com>
2022-10-15 05:01:38 -0700

    task quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
-#include <unlstd.h>
-
+#include <unistd.h>
 /**
- * main - print exactly and that piece of art is useful
+* main - main function in the task
  *
- * Return: Always 0 (success)
+ *
+ * *Return: return 1
+ *
  */
 
 int main(void)
+
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+
+		write(2, "and that piece of artis useful\" - Dora Korpar, 2015-10-19\n", 59);
+			return (1);
+
 }
