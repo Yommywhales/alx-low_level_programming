fd0e5de08e1ca4452b6c2c55fc07e28ebabfbe54
Yommywhales <yommywhales@gmail.com>
2022-10-15 02:20:27 -0700

    task four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
-#include <string.h>
-
 /**
- * main - Main function of my line
+ * main -print a line of ci=ode using puts
  *
- *
- * Return: return 0 everytime
+ * Return: 0
  */
+
 int main(void)
 {
-
-	char displayed[52]
-
-strcpy(displayed, "\"Programming is like building a multilingual puzzle");
-	puts('displayed');
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
