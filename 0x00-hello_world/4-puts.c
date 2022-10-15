539bf7ba06f2d2a87540f69be0d5995d057064b1
Yommywhales <yommywhales@gmail.com>
2022-10-15 01:59:19 -0700

    task four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+
 /**
- * main - print a line of code using puts
-*
- *Return: 0
+ * main - Main function of my line
+ *
+ *
+ * Return: return 0 everytime
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+
+	char displayed[52]
+
+		strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+	puts(displayed);
 	return (0);
 }
