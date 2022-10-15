d974c5087f6b7d9672f0eb59b8bfab9f706251f3
Yommywhales <yommywhales@gmail.com>
2022-10-15 02:08:34 -0700

    task four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -12,7 +12,7 @@ int main(void)
 
 	char displayed[52]
 
-		strcpy(displayed, "\"Programming is like building a multilingual puzzle");
-	puts(displayed);
+strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+	puts('displayed');
 	return (0);
 }
