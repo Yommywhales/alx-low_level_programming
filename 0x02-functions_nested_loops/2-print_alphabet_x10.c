2318677d0c44d426db07b67b52f24ef2492de8b5
Yommywhales <yommywhales@gmail.com>
2022-10-19 16:40:46 -0700

    two

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,20 @@
 #include "main.h"
 
 /**
- *
- * *prints_alphabets -> lower case
- *
- *
+ *prints_alphabets -> lower case
  */
 void print_alphabet_x10(void)
 {
 	int j;
 	int p;
+
 	for (p = '0' ; p < 10 ; j++)
-	  {
-		  for (j = 'a' ; j <= 'z' ; j++)
-		  {
+	{
+		for (j = 'a' ; j <= 'z' ; j++)
+		{
 		 _putchar(j);
-	  }
+		}
 	_putchar('\n');
-	  }
+	}
 
 }
