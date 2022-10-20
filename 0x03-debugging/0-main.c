1524dfc1fd6b64bb3f222d7f60837c53fe7bd3cc
Yommywhales <yommywhales@gmail.com>
2022-10-20 10:01:07 -0700

    task zero

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * main - test function that prints if integer is positive or negative
+ * main - test function for positve or negative
  * Return: 0
  */
 
